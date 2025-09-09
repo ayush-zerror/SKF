635db466d6881a1572006d1e0ac052d14941515d
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-09-09 16:23:36 +0530

    update movie images

--- src/pages/movies/index.jsx
+++ src/pages/movies/index.jsx
@@ -1,28 +1,16 @@
 import MoviesListing from "@/components/common/movieListing/MoviesListing";
 import React from "react";
 const movies = [
-  { image: "/images/home/movie1.png", title: "[1] Sikandar", year: "2025" },
-  { image: "/images/home/movie2.png", title: "[2] Farrey", year: "2024" },
-  {
-    image: "/images/home/movie3.png",
-    title: "[3] Kisi Ka Bhai Kisi Ki Jaan",
-    year: "2023",
-  },
-  { image: "/images/home/movie1.png", title: "[1] Sikandar", year: "2025" },
-  { image: "/images/home/movie2.png", title: "[2] Farrey", year: "2024" },
-  {
-    image: "/images/home/movie3.png",
-    title: "[3] Kisi Ka Bhai Kisi Ki Jaan",
-    year: "2023",
-  },
-  { image: "/images/home/movie1.png", title: "[1] Sikandar", year: "2025" },
-  { image: "/images/home/movie2.png", title: "[2] Farrey", year: "2024" },
-  {
-    image: "/images/home/movie3.png",
-    title: "[3] Kisi Ka Bhai Kisi Ki Jaan",
-    year: "2023",
-  },
-  { image: "/images/home/movie1.png", title: "[1] Sikandar", year: "2025" },
+  { image: "/images/movie/movie1.png", title: "[1] Sikandar", year: "2025" },
+  { image: "/images/movie/movie2.png", title: "[2] Farrey", year: "2024" },
+  { image: "/images/movie/movie3.png", title: "[3] Chillar Party", year: "2011" },
+  { image: "/images/movie/movie4.png", title: "[4] dr.cabbie", year: "2014" },
+  { image: "/images/movie/movie5.png", title: "[5] Bajrangi Bhaijaan", year: "2015" },
+  { image: "/images/movie/movie6.png", title: "[6] Hero", year: "2015" },
+  { image: "/images/movie/movie7.png", title: "[7] Tubelight", year: "2017" },
+  { image: "/images/movie/movie8.png", title: "[8] race 3", year: "2018" },
+  { image: "/images/movie/movie9.png", title: "[9] loveyatri", year: "2025" },
+  { image: "/images/movie/movie10.png", title: "[10] Farrey", year: "2024" },
 ];
 const Movies = () => {
   return (
