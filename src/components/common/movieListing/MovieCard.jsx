8d2190e56d166e6f8571926e65898f44f073ae5d
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-09-12 14:52:54 +0530

    card hover

--- src/components/common/movieListing/MovieCard.jsx
+++ src/components/common/movieListing/MovieCard.jsx
@@ -4,14 +4,9 @@ import React from "react";
 
 const MovieCard = ({ data }) => {
   return (
-    <Link href='/movies/1' className="movie_card">
+    <Link href="/movies/1" className="movie_card">
       <div className="movie_img">
-        <Image
-          width={1000}
-          height={1000}
-          src={data?.image}
-          alt={data?.title}
-        />
+        <Image width={1000} height={1000} src={data?.image} alt={data?.title} />
       </div>
       <div className="movie_dets">
         <span>{data?.title}</span>
